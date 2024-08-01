b71e1b87377e045e6f21c11a7c2f35e8d5050d3f
Bruno Coelho <bbcoelho@gmail.com>
2024-08-01 11:11:37 -0300

    feat: update summary methods

--- gas/server/src/Summary.ts
+++ gas/server/src/Summary.ts
@@ -8,6 +8,40 @@ class Summary {
         this.accountId = accountId;
     }
 
+    getResult(): string {
+        return JSON.stringify(this.result);
+    }
+
+    done(msg?: string): this {
+        if (msg) {
+            this.result = msg;
+            return this;
+        }
+        this.result = `Done! ${JSON.stringify(this.result)}`;
+        return this;
+    }
+
+    rebuild(): this {
+        this.result = 'Account needs rebuild: reseting async...';
+        return this;
+    }
+
+    resetingAsync(): this {
+        this.result = 'Reseting async...';
+        return this;
+    }
+
+    calculatingAsync(): this {
+        this.result = 'Calculating async...';
+        return this;
+    }
+
+    lockError(): this {
+        this.error = true;
+        this.result = 'Cannot proceed: collection has locked/closed book(s)';
+        return this;
+    }
+
     json(): this {
         this.result = JSON.stringify(this.result);
         return this;
