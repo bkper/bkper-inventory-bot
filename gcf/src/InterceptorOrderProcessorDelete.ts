22342939507b3ab70d0d5767eeb1f738fcd54793
Bruno Coelho <bbcoelho@gmail.com>
2024-08-06 11:20:19 -0300

    feat: cascade delete transactions

--- gcf/src/InterceptorOrderProcessorDelete.ts
+++ gcf/src/InterceptorOrderProcessorDelete.ts
@@ -3,31 +3,38 @@ import { uncheckAndRemove } from "./BotService";
 
 export abstract class InterceptorOrderProcessorDelete {
 
-    protected cascadeDelete(book: Book, transaction: bkper.Transaction) {
-      if (!book) {
-        return;
-      }
-      // this.cascadeDeleteTransactions(book, transaction, ``);
-      // this.cascadeDeleteTransactions(book, transaction, `additional_cost_`);
-      // this.cascadeDeleteTransactions(getBaseBook(book), transaction, `fx_`);
-    }
-  
-    protected async cascadeDeleteTransactions(book: Book, remoteTx: bkper.Transaction, prefix: string) {
+	protected async cascadeDelete(book: Book, transaction: bkper.Transaction): Promise<Transaction> {
+		if (!book) {
+			return null;
+		}
+		return await this.cascadeDeleteTransactions(book, transaction, ``);
+	}
 
-    }
+	protected async cascadeDeleteTransactions(book: Book, remoteTx: bkper.Transaction, prefix: string): Promise<Transaction> {
+		const iterator = book.getTransactions(`remoteId:${prefix}${remoteTx.id}`);
+		if (await iterator.hasNext()) {
+			let tx = await iterator.next();
+			if (tx.isChecked()) {
+				tx = await tx.uncheck();
+			}
+			const response = await tx.remove();
+			return response;
+		}
+		return null;
+	}
 
-    protected async buildDeleteResponse(tx: Transaction): Promise<string> {
-        return `DELETED: ${tx.getDateFormatted()} ${tx.getAmount()} ${await tx.getCreditAccountName()} ${await tx.getDebitAccountName()} ${tx.getDescription()}`
-    }
+	protected async buildDeleteResponse(tx: Transaction): Promise<string> {
+		return `DELETED: ${tx.getDateFormatted()} ${tx.getAmount()} ${await tx.getCreditAccountName()} ${await tx.getDebitAccountName()} ${tx.getDescription()}`
+	}
 
-    protected async deleteTransactionByRemoteId(book: Book, remoteId: string): Promise<Transaction> {
-        let iterator = book.getTransactions(`remoteId:${remoteId}`);
-        if (await iterator.hasNext()) {
-            let tx = await iterator.next();
-            tx = await uncheckAndRemove(tx);
-            return tx;
-        }
-        return null;
-    }
+	protected async deleteTransactionByRemoteId(book: Book, remoteId: string): Promise<Transaction> {
+		let iterator = book.getTransactions(`remoteId:${remoteId}`);
+		if (await iterator.hasNext()) {
+			let tx = await iterator.next();
+			tx = await uncheckAndRemove(tx);
+			return tx;
+		}
+		return null;
+	}
 
 }
--- gcf/src/InterceptorOrderProcessorDeleteGoods.ts
+++ gcf/src/InterceptorOrderProcessorDeleteGoods.ts
@@ -1,31 +1,35 @@
 import { Book } from "bkper";
 import { Result } from ".";
 import { InterceptorOrderProcessorDelete } from "./InterceptorOrderProcessorDelete";
-import { getExchangeCodeFromAccount, getFinancialBook, getGoodAccount } from "./BotService";
+import { buildBookAnchor, getExchangeCodeFromAccount, getFinancialBook, getGoodAccount } from "./BotService";
 
 export class InterceptorOrderProcessorDeleteGoods extends InterceptorOrderProcessorDelete {
 
     async intercept(inventoryBook: Book, event: bkper.Event): Promise<Result> {
 
-        let operation = event.data.object as bkper.TransactionOperation;
-        let transactionPayload = operation.transaction;
+        const operation = event.data.object as bkper.TransactionOperation;
+        const transactionPayload = operation.transaction;
 
         if (!transactionPayload.posted) {
-            return {result: false};
+            return { result: false };
         }
 
-        let goodTx = await inventoryBook.getTransaction(transactionPayload.id);
-        let goodAccount = await getGoodAccount(goodTx);
+        const goodTx = await inventoryBook.getTransaction(transactionPayload.id);
+        const goodAccount = await getGoodAccount(goodTx);
         if (!goodAccount) {
-            return {result: false};
+            return { result: false };
         }
 
-        let goodExcCode = await getExchangeCodeFromAccount(goodAccount);
-        let financialBook = await getFinancialBook(inventoryBook, goodExcCode);
+        const goodExcCode = await getExchangeCodeFromAccount(goodAccount);
+        const financialBook = await getFinancialBook(inventoryBook, goodExcCode);
 
-        this.cascadeDelete(financialBook, transactionPayload);
+        const response = await this.cascadeDelete(financialBook, transactionPayload);
+        if (response) {
+            const bookAnchor = buildBookAnchor(financialBook);
+            return { result: `${bookAnchor}: ${await this.buildDeleteResponse(response)}` };
+        }
 
-        return {result: `DELETED: ${goodTx.getDateFormatted()} ${goodTx.getAmount()} ${await goodTx.getCreditAccountName()} ${await goodTx.getDebitAccountName()} ${goodTx.getDescription()}`};
+        return { result: false };
     }
 
 }
