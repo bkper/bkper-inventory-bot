87d7942141f0b4b6d6d2964bc8cd079cd92a87fb
Bruno Coelho <bbcoelho@gmail.com>
2024-10-01 17:01:50 -0300

    refact: remove ts-nocheck and refact issues

--- gcf/src/EventHandlerTransactionPosted.ts
+++ gcf/src/EventHandlerTransactionPosted.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { Bkper } from "bkper-js";
 import { Result } from "./index.js";
 import { InterceptorOrderProcessor } from "./InterceptorOrderProcessor.js";
@@ -8,7 +7,7 @@ export class EventHandlerTransactionPosted {
   async handleEvent(event: bkper.Event): Promise<Result> {
 
     // TODO: use "baseBook"? will model have a base book?
-    let baseBook = await Bkper.getBook(event.bookId);
+    let baseBook = await Bkper.getBook(event.bookId!);
     const response = await new InterceptorOrderProcessor().intercept(baseBook, event)
     if (response) {
       return response;
