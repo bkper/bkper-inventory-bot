f259ebfc998e81bb8fa93a59c4af34b50690feeb
Bruno Coelho <bbcoelho@gmail.com>
2024-11-07 17:15:57 -0300

    refact: adapt to ts:strict

--- gcf/src/InterceptorFlagRebuild.ts
+++ gcf/src/InterceptorFlagRebuild.ts
@@ -7,19 +7,21 @@ export class InterceptorFlagRebuild {
 
     async intercept(baseBook: Book, event: bkper.Event): Promise<Result> {
         if (isInventoryBook(baseBook) && event.agent?.id != 'inventory-bot') {
-            if (!event.data?.object) {
-                return { result: false };
-            }
-            let operation = event.data.object as bkper.TransactionOperation;
-            let transactionPayload = operation.transaction;
-            let transaction = await baseBook.getTransaction(transactionPayload!.id!);
-            
-            let goodAccount = transaction ? await getGoodAccount(transaction) : null;
-            
-            if (goodAccount && goodAccount.getProperty(NEEDS_REBUILD_PROP) == null) {
-                goodAccount.setProperty(NEEDS_REBUILD_PROP, 'TRUE').update();
-                const msg = `Flagging account ${goodAccount.getName()} for rebuild`;
-                return { warning: msg, result: msg };
+            if (event.data) {
+                if (!event.data.object) {
+                    return { result: false };
+                }
+                let operation = event.data.object as bkper.TransactionOperation;
+                let transactionPayload = operation.transaction;
+                let transaction = await baseBook.getTransaction(transactionPayload!.id!);
+                
+                let goodAccount = transaction ? await getGoodAccount(transaction) : null;
+                
+                if (goodAccount && goodAccount.getProperty(NEEDS_REBUILD_PROP) == null) {
+                    goodAccount.setProperty(NEEDS_REBUILD_PROP, 'TRUE').update();
+                    const msg = `Flagging account ${goodAccount.getName()} for rebuild`;
+                    return { warning: msg, result: msg };
+                }
             }
         }
         return { result: false };
