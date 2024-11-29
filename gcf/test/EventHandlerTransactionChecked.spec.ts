164e5088fd8436f074d8ebf55ffa3422a5426c31
Bruno Coelho <bbcoelho@gmail.com>
2024-11-29 14:02:56 -0300

    test: ensure getTransactionQuery() returns the remoteId of the purchase transaction in the Inventory Book

--- gcf/test/EventHandlerTransactionChecked.spec.ts
+++ gcf/test/EventHandlerTransactionChecked.spec.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { AccountType } from 'bkper-js';
 
 import { EventHandlerTransactionChecked } from '../dist/EventHandlerTransactionChecked.js';
+import { PURCHASE_CODE_PROP } from '../dist/constants.js';
 
 describe('EventHandlerTransactionChecked', () => {
 
@@ -20,6 +21,27 @@ describe('EventHandlerTransactionChecked', () => {
             const query = new EventHandlerTransactionChecked().getTransactionQuery(transaction);
 
             expect(query).to.equal(`remoteId:123`);
+        });
+    });
+
+    describe('#getTransactionQuery(transaction: bkper.Transaction): string', () => {
+        it('should return the remoteId of the purchase transaction in the Inventory Book', () => {
+
+            const transaction = {
+                id: '123',
+                creditAccount: {
+                },
+                debitAccount: {
+                    normalizedName: 'product_a',
+                },
+                properties: {
+                    [PURCHASE_CODE_PROP]: '12B3C4'
+                },
+            };
+
+            const query = new EventHandlerTransactionChecked().getTransactionQuery(transaction);
+
+            expect(query).to.equal(`remoteId:12B3C4_product_a`);
         });
     });
 
