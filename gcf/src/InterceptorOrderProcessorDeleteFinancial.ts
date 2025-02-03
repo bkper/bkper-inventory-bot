231c2db13793c51d17d50d7779615ffde92c75ac
Bruno Coelho <bbcoelho@gmail.com>
2025-02-03 18:51:50 -0300

    refact: remove no longer used flows

--- gcf/src/InterceptorOrderProcessorDeleteFinancial.ts
+++ gcf/src/InterceptorOrderProcessorDeleteFinancial.ts
@@ -1,7 +1,7 @@
 import { AccountType, Amount, Book, Transaction } from "bkper-js";
 import { InterceptorOrderProcessorDelete } from "./InterceptorOrderProcessorDelete.js";
 import { Result } from "./index.js";
-import { ADDITIONAL_COST_PROP, ADDITIONAL_COST_TX_IDS, GOOD_PROP, PURCHASE_CODE_PROP, PURCHASE_INVOICE_PROP, TOTAL_ADDITIONAL_COSTS_PROP, TOTAL_COST_PROP } from "./constants.js";
+import { ADDITIONAL_COST_PROP, ADDITIONAL_COST_TX_IDS, GOOD_PROP, PURCHASE_CODE_PROP, PURCHASE_INVOICE_PROP, QUANTITY_PROP, TOTAL_ADDITIONAL_COSTS_PROP, TOTAL_COST_PROP } from "./constants.js";
 import { buildBookAnchor, flagInventoryAccountForRebuildIfNeeded, getGoodPurchaseRootTx, getInventoryBook } from "./BotService.js";
 
 export class InterceptorOrderProcessorDeleteFinancial extends InterceptorOrderProcessorDelete {
@@ -20,6 +20,12 @@ export class InterceptorOrderProcessorDeleteFinancial extends InterceptorOrderPr
         let responses: string[] = [];
 
         if (transactionPayload && transactionPayload.properties && transactionPayload.debitAccount && transactionPayload.id) {
+
+            // deleted transaction is the purchase transaction
+            if (transactionPayload.properties[QUANTITY_PROP] != undefined && (transactionPayload.properties[PURCHASE_CODE_PROP] == transactionPayload.properties[PURCHASE_INVOICE_PROP])) {
+
+            }
+
             // deleted transaction is the sale transaction
             if (transactionPayload.properties[GOOD_PROP] != undefined && transactionPayload.debitAccount.type == AccountType.INCOMING) {
                 const deletedTxs = await this.deleteOnInventoryBook(financialBook, transactionPayload.id);
@@ -28,34 +34,6 @@ export class InterceptorOrderProcessorDeleteFinancial extends InterceptorOrderPr
                     responses = responses.concat(await this.buildResults(deletedTxs));
                     if (rebuildFlagMsg) {
                         responses.push(rebuildFlagMsg);
-                    }
-                }
-            }
-
-            // deleted transaction is the root good purchase transaction
-            // else if (transactionPayload.properties[GOOD_PROP] != undefined && (transactionPayload.properties[PURCHASE_CODE_PROP] == transactionPayload.properties[PURCHASE_INVOICE_PROP])) {
-            //     // Delete additional cost transactions posted by the user
-            //     responses = responses.concat(await this.deleteAddCostRootTransactions(financialBook, transactionPayload))
-            //     // Delete good purchase transactions posted by the bot (from buyer to good account)
-            //     const response = await this.deleteTransactionByRemoteId(financialBook, `${GOOD_PROP}_${transactionPayload.id}`);
-            //     if (response) {
-            //         responses.push(await this.buildDeleteResponse(response));
-            //         // delete transaction in the inventory book
-            //         const response2 = await this.deleteOnInventoryBook(financialBook, response.getId());
-            //         if (response2) {
-            //             responses.push(await this.buildDeleteResponse(response2));
-            //         }
-            //     }
-            // }
-
-            // deleted transaction is the root additional cost transaction posted by the user (from supplier to buyer)
-            else if (transactionPayload.properties[GOOD_PROP] != undefined && (transactionPayload.properties[PURCHASE_CODE_PROP] != transactionPayload.properties[PURCHASE_INVOICE_PROP])) {
-                if (transactionPayload.amount) {
-                    const response = await this.deleteTransactionByRemoteId(financialBook, `${ADDITIONAL_COST_PROP}_${transactionPayload.id}`);
-                    if (response) {
-                        responses.push(await this.buildDeleteResponse(response));
-                        responses.push(await this.removeAdditionalCostFromGoodTx(financialBook, transactionPayload.properties[PURCHASE_CODE_PROP], transactionPayload.id));
-                        responses.push(await this.subtractAdditionalCostFromInventoryTx(financialBook, transactionPayload.properties[PURCHASE_CODE_PROP], transactionPayload.amount));
                     }
                 }
             }
@@ -76,64 +54,11 @@ export class InterceptorOrderProcessorDeleteFinancial extends InterceptorOrderPr
                     }
                 }
             }
-            
+
             return { result: responses.length > 0 ? responses : false };
         }
+
         return { result: false };
-    }
-
-    private async removeAdditionalCostFromGoodTx(financialBook: Book, purchaseCodeProp: string, rootAdditionalCostTxId: string): Promise<string> {
-        let rootPurchaseTx = await getGoodPurchaseRootTx(financialBook, purchaseCodeProp);
-        if (rootPurchaseTx) {
-            let additionalCostTxIds = rootPurchaseTx.getProperty(ADDITIONAL_COST_TX_IDS);
-            if (additionalCostTxIds) {
-                let remoteIds: string[] = JSON.parse(additionalCostTxIds);
-                const index = remoteIds.indexOf(rootAdditionalCostTxId);
-                if (index != -1) {
-                    remoteIds.splice(index, 1);
-                }
-                additionalCostTxIds = remoteIds.length > 0 ? JSON.stringify(remoteIds) : '';
-
-                await rootPurchaseTx.setProperty(ADDITIONAL_COST_TX_IDS, additionalCostTxIds).update();
-                return `UPDATED: good purchase transaction on ${rootPurchaseTx.getDate()}, purchase_code: ${purchaseCodeProp}, ${rootPurchaseTx.getDescription()}`;
-            } else {
-                return `ERROR: transaction not found in add_cost_transactions`;
-            }
-        } else {
-            return `ERROR: transaction not found in add_cost_transactions`;
-        }
-    }
-
-    private async subtractAdditionalCostFromInventoryTx(financialBook: Book, purchaseCodeProp: string, value: string): Promise<string> {
-        const inventoryBook = getInventoryBook(financialBook);
-        if (inventoryBook) {
-            let bookAnchor = buildBookAnchor(inventoryBook);
-            let goodTransaction = (await inventoryBook.listTransactions(`remoteId:${purchaseCodeProp}`)).getFirst();
-            const totalCostProp = goodTransaction?.getProperty(TOTAL_COST_PROP);
-            const totalAdditionalCostsProp = goodTransaction?.getProperty(TOTAL_ADDITIONAL_COSTS_PROP);
-            if (goodTransaction) {
-                if (totalCostProp && totalAdditionalCostsProp) {
-                    // update additional cost and total cost properties in inventory book transaction
-                    const currentTotalCost = new Amount(totalCostProp);
-                    const currentAdditionalCosts = new Amount(totalAdditionalCostsProp);
-    
-                    const costToSubtract = new Amount(value);
-    
-                    const newTotalCost = currentTotalCost.minus(costToSubtract);
-                    const newAdditionalCosts = currentAdditionalCosts.minus(costToSubtract);
-    
-                    goodTransaction.setProperty(TOTAL_ADDITIONAL_COSTS_PROP, newAdditionalCosts.toString()).setProperty(TOTAL_COST_PROP, newTotalCost.toString()).update();
-    
-                    let record = `${goodTransaction.getDate()} ${goodTransaction.getAmount()} ${await goodTransaction.getCreditAccountName()} ${await goodTransaction.getDebitAccountName()} ${goodTransaction.getDescription()}`;
-                    return `UPDATED: ${bookAnchor}: ${record}`;
-                } else {
-                    return 'ERROR (subtractAdditionalCostFromInventoryTx): additional_costs or total_cost properties not found';
-                }
-            } else {
-                return `PURCHASE TRANSACTION NOT FOUND IN BOOK ${bookAnchor}`
-            }
-        }
-        return 'ERROR (subtractAdditionalCostFromInventoryTx): Inventory book not found';
     }
 
     protected async deleteOnInventoryBook(financialBook: Book, remoteId: string): Promise<Transaction[] | undefined> {
