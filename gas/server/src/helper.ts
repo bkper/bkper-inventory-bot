18a8b8de816cf01db77d539ac8742705109150bc
Bruno Coelho <bbcoelho@gmail.com>
2025-02-13 16:34:15 -0300

    feat: add optional afterDate parameter to getAccountQuery

--- gas/server/src/helper.ts
+++ gas/server/src/helper.ts
@@ -1,7 +1,11 @@
 namespace helper {
 
-    export function getAccountQuery(goodAccountName: string, beforeDate?: string): string {
+    export function getAccountQuery(goodAccountName: string, beforeDate?: string, afterDate?: string): string {
         let query = `account:'${goodAccountName}'`;
+
+        if (afterDate) {
+            query += ` after:${afterDate}`
+        }
 
         if (beforeDate) {
             query += ` before:${beforeDate}`
