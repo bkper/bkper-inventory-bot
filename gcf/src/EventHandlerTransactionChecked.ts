029470bbbe78fe87adfc9ac9d247338b98adba48
Bruno Coelho <bbcoelho@gmail.com>
2025-02-28 14:55:17 -0300

    feat: create inventory account if need

--- gcf/src/EventHandlerTransactionChecked.ts
+++ gcf/src/EventHandlerTransactionChecked.ts
@@ -1,6 +1,6 @@
 import { Account, AccountType, Amount, Book, Group, Transaction } from "bkper-js";
 import { EventHandlerTransaction } from "./EventHandlerTransaction.js";
-import { buildBookAnchor, getCOGSCalculationDateValue, getGoodExchangeCodeFromAccount, getQuantity } from "./BotService.js";
+import { buildBookAnchor, getCOGSCalculationDateValue, getQuantity } from "./BotService.js";
 import { GOOD_BUY_ACCOUNT_NAME, GOOD_EXC_CODE_PROP, GOOD_PROP, GOOD_PURCHASE_COST_PROP, GOOD_SELL_ACCOUNT_NAME, NEEDS_REBUILD_PROP, ORDER_PROP, ORIGINAL_QUANTITY_PROP, PURCHASE_CODE_PROP, PURCHASE_INVOICE_PROP, SALE_AMOUNT_PROP, SALE_INVOICE_PROP, TOTAL_COST_PROP } from "./constants.js";
 import { Result } from "./index.js";
 import { InterceptorFlagRebuild } from "./InterceptorFlagRebuild.js";
@@ -20,7 +20,7 @@ export class EventHandlerTransactionChecked extends EventHandlerTransaction {
     }
 
     // create purchase (Buy) or sale (Sell) transactions in the inventory book in response to the financial transactions
-    protected async connectedTransactionNotFound(inventoryBook: Book, financialTransaction: bkper.Transaction, goodExcCode: string): Promise<string | undefined> {
+    protected async connectedTransactionNotFound(inventoryBook: Book, financialBook: Book, financialTransaction: bkper.Transaction, goodExcCode: string): Promise<string | undefined> {
         if (financialTransaction.creditAccount && financialTransaction.debitAccount && financialTransaction.date && financialTransaction.id && financialTransaction.properties) {
 
             const quantity = getQuantity(financialTransaction);
@@ -29,9 +29,13 @@ export class EventHandlerTransactionChecked extends EventHandlerTransaction {
             const saleInvoice = financialTransaction.properties[SALE_INVOICE_PROP];
 
             // prevent bot response when transaction is missing quantity or is not a purchase or sale transaction
-            if (quantity == undefined || quantity.eq(0) || (financialCreditAccount.type != AccountType.ASSET && financialCreditAccount.type != AccountType.INCOMING)) {
+            if (quantity == undefined || quantity.eq(0)) {
                 return undefined;
             }
+            // if (quantity == undefined || quantity.eq(0) || (financialCreditAccount.type != AccountType.ASSET && financialCreditAccount.type != AccountType.INCOMING)) {
+            //     return undefined;
+            // }
+
             if (purchaseInvoice == undefined) {
                 // it's not a purchase transaction
                 if (saleInvoice == undefined || financialCreditAccount.type != AccountType.INCOMING) {
@@ -40,19 +44,34 @@ export class EventHandlerTransactionChecked extends EventHandlerTransaction {
                 }
             }
 
-            let goodAccount = await inventoryBook.getAccount(financialTransaction.properties?.[GOOD_PROP]);
-            if (goodAccount) {
+
+            if (financialTransaction.properties?.[SALE_INVOICE_PROP]) {
                 // Selling
-                let goodSellAccount = await inventoryBook.getAccount(GOOD_SELL_ACCOUNT_NAME);
-                if (goodSellAccount == undefined) {
-                    goodSellAccount = await new Account(inventoryBook).setName(GOOD_SELL_ACCOUNT_NAME).setType(AccountType.OUTGOING).create();
+                const goodProperty = financialTransaction.properties?.[GOOD_PROP];
+
+                let inventoryAccount = await inventoryBook.getAccount(goodProperty);
+                if (!inventoryAccount) {
+                    inventoryAccount = await this.createConnectedInventoryAccount(inventoryBook, goodProperty);
+                }
+
+                let financialAssetAccount = await financialBook.getAccount(goodProperty);
+                if (!financialAssetAccount) {
+                    // create financial asset account to be used afterwards by FIFO
+                    // await this.createConnectedFinancialAssetAccount(financialBook, goodProperty);
+                }
+                
+                console.log('SELL: inventoryAccount', inventoryAccount.getName(), inventoryAccount.getType());
+
+                let inventorySellAccount = await inventoryBook.getAccount(GOOD_SELL_ACCOUNT_NAME);
+                if (!inventorySellAccount) {
+                    inventorySellAccount = await new Account(inventoryBook).setName(GOOD_SELL_ACCOUNT_NAME).setType(AccountType.OUTGOING).create();
                 }
 
                 const newTransaction = await new Transaction(inventoryBook)
                     .setDate(financialTransaction.date)
                     .setAmount(quantity)
-                    .setCreditAccount(goodAccount)
-                    .setDebitAccount(goodSellAccount)
+                    .setCreditAccount(inventoryAccount)
+                    .setDebitAccount(inventorySellAccount)
                     .setDescription(financialTransaction.description ?? '')
                     .addRemoteId(financialTransaction.id)
                     .setProperty(SALE_INVOICE_PROP, financialTransaction.properties[SALE_INVOICE_PROP])
@@ -63,50 +82,54 @@ export class EventHandlerTransactionChecked extends EventHandlerTransaction {
                     ;
 
                 const inventoryBookAnchor = buildBookAnchor(inventoryBook);
-                const record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${goodAccount.getName()} ${goodSellAccount.getName()} ${newTransaction.getDescription()}`;
-                const needsRebuild = await this.checkLastTxDate(goodAccount, financialTransaction);
+                const record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${inventoryAccount.getName()} ${inventoryAccount.getName()} ${newTransaction.getDescription()}`;
+                const needsRebuild = await this.checkLastTxDate(inventoryAccount, financialTransaction);
 
                 if (needsRebuild) {
-                    return `SELL: ${inventoryBookAnchor}: ${record} / WARNING: Transaction date is before the last COGS calculation date. Flagging account ${goodAccount.getName()} for rebuild`;
+                    return `SELL: ${inventoryBookAnchor}: ${record} / WARNING: Transaction date is before the last COGS calculation date. Flagging account ${inventoryAccount.getName()} for rebuild`;
                 } else {
                     return `SELL: ${inventoryBookAnchor}: ${record}`;
                 }
 
-            } else {
+            } else if (financialTransaction.properties?.[PURCHASE_INVOICE_PROP]) {
+                // Buying
                 const financialDebitAccount = financialTransaction.debitAccount;
-                goodAccount = await this.getConnectedGoodAccount(inventoryBook, financialDebitAccount);
-                if (goodAccount) {
-                    // Buying
-                    let goodBuyAccount = await inventoryBook.getAccount(GOOD_BUY_ACCOUNT_NAME);
-                    if (goodBuyAccount == null) {
-                        goodBuyAccount = await new Account(inventoryBook).setName(GOOD_BUY_ACCOUNT_NAME).setType(AccountType.INCOMING).create();
-                    }
+                let inventoryAccount = await inventoryBook.getAccount(financialDebitAccount.name);
+                if (!inventoryAccount) {
+                    inventoryAccount = await this.createConnectedInventoryAccount(inventoryBook, financialDebitAccount);
+                }
 
-                    const newTransaction = await new Transaction(inventoryBook)
-                        .setDate(financialTransaction.date)
-                        .setAmount(quantity)
-                        .setCreditAccount(goodBuyAccount)
-                        .setDebitAccount(goodAccount)
-                        .setDescription(financialTransaction.description ?? '')
-                        .addRemoteId(financialTransaction.id)
-                        .setProperty(PURCHASE_CODE_PROP, financialTransaction.properties[PURCHASE_CODE_PROP])
-                        .setProperty(ORIGINAL_QUANTITY_PROP, quantity.toString())
-                        .setProperty(GOOD_PURCHASE_COST_PROP, new Amount(financialTransaction.amount ?? 0).toString())
-                        .setProperty(ORDER_PROP, financialTransaction.properties[ORDER_PROP])
-                        .setProperty(GOOD_EXC_CODE_PROP, goodExcCode)
-                        .setProperty(TOTAL_COST_PROP, new Amount(financialTransaction.amount ?? 0).toString())
-                        .post()
-                        ;
+                console.log('BUY: inventoryAccount', inventoryAccount.getName(), inventoryAccount.getType());
 
-                    const inventoryBookAnchor = buildBookAnchor(inventoryBook);
-                    const record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${goodBuyAccount.getName()} ${goodAccount.getName()} ${newTransaction.getDescription()}`;
-                    const needsRebuild = await this.checkLastTxDate(goodAccount, financialTransaction);
+                let inventoryBuyAccount = await inventoryBook.getAccount(GOOD_BUY_ACCOUNT_NAME);
+                if (inventoryBuyAccount == null) {
+                    inventoryBuyAccount = await new Account(inventoryBook).setName(GOOD_BUY_ACCOUNT_NAME).setType(AccountType.INCOMING).create();
+                }
 
-                    if (needsRebuild) {
-                        return `BUY: ${inventoryBookAnchor}: ${record} / WARNING: Transaction date is before the last COGS calculation date. Flagging account ${goodAccount.getName()} for rebuild`;
-                    } else {
-                        return `BUY: ${inventoryBookAnchor}: ${record}`;
-                    }
+                const newTransaction = await new Transaction(inventoryBook)
+                    .setDate(financialTransaction.date)
+                    .setAmount(quantity)
+                    .setCreditAccount(inventoryBuyAccount)
+                    .setDebitAccount(inventoryAccount)
+                    .setDescription(financialTransaction.description ?? '')
+                    .addRemoteId(financialTransaction.id)
+                    .setProperty(PURCHASE_CODE_PROP, financialTransaction.properties[PURCHASE_CODE_PROP])
+                    .setProperty(ORIGINAL_QUANTITY_PROP, quantity.toString())
+                    .setProperty(GOOD_PURCHASE_COST_PROP, new Amount(financialTransaction.amount ?? 0).toString())
+                    .setProperty(ORDER_PROP, financialTransaction.properties[ORDER_PROP])
+                    .setProperty(GOOD_EXC_CODE_PROP, goodExcCode)
+                    .setProperty(TOTAL_COST_PROP, new Amount(financialTransaction.amount ?? 0).toString())
+                    .post()
+                    ;
+
+                const inventoryBookAnchor = buildBookAnchor(inventoryBook);
+                const record = `${newTransaction.getDate()} ${newTransaction.getAmount()} ${inventoryBuyAccount.getName()} ${inventoryAccount.getName()} ${newTransaction.getDescription()}`;
+                const needsRebuild = await this.checkLastTxDate(inventoryAccount, financialTransaction);
+
+                if (needsRebuild) {
+                    return `BUY: ${inventoryBookAnchor}: ${record} / WARNING: Transaction date is before the last COGS calculation date. Flagging account ${inventoryAccount.getName()} for rebuild`;
+                } else {
+                    return `BUY: ${inventoryBookAnchor}: ${record}`;
                 }
             }
         }
@@ -124,41 +147,51 @@ export class EventHandlerTransactionChecked extends EventHandlerTransaction {
     }
 
     // returns the good account from the inventory book corresponding to the good account in the financial book (creates the account if it doesn't exist)
-    private async getConnectedGoodAccount(inventoryBook: Book, financialAccount: bkper.Account): Promise<Account | undefined> {
-        const goodExchangeCode = getGoodExchangeCodeFromAccount(financialAccount);
-        if (goodExchangeCode != undefined && financialAccount.name && financialAccount.type && financialAccount.properties) {
-            let goodAccount = await inventoryBook.getAccount(financialAccount.name);
-            if (goodAccount == undefined) {
-                goodAccount = new Account(inventoryBook)
-                    .setName(financialAccount.name)
-                    .setType(financialAccount.type as AccountType)
-                    .setProperties(financialAccount.properties)
-                    .setArchived(financialAccount.archived ?? false);
-                if (financialAccount.groups) {
-                    for (const financialGroup of financialAccount.groups) {
-                        if (financialGroup && financialGroup.properties && financialGroup.name && financialGroup.hidden) {
-                            let goodGroup = await inventoryBook.getGroup(financialGroup.name);
-                            const goodExcCode = financialGroup.properties[GOOD_EXC_CODE_PROP];
-                            if (goodGroup == undefined && goodExcCode != undefined && goodExcCode.trim() != '') {
-                                goodGroup = await new Group(inventoryBook)
-                                    .setHidden(financialGroup.hidden)
-                                    .setName(financialGroup.name)
-                                    .setProperties(financialGroup.properties)
-                                    .create()
-                                    ;
-                            }
-                            if (goodGroup) {
-                                goodAccount.addGroup(goodGroup);
-                            }
-                        }
-                    }
-                }
-                goodAccount = await goodAccount.create();
-            }
-            return goodAccount;
+    private async createConnectedInventoryAccount(inventoryBook: Book, financialAccount: bkper.Account | string): Promise<Account> {
+
+        let inventoryAccountName: string;
+        let inventoryAccountProperties: { [key: string]: string; } = {};
+        let inventoryAccountArchived: boolean;
+        let inventoryAccountGroups: bkper.Group[] = [];
+
+        if (typeof (financialAccount) === 'string') {
+            inventoryAccountName = financialAccount;
+            inventoryAccountProperties = {};
+            inventoryAccountArchived = false;
+            inventoryAccountGroups = [];
+        } else {
+            inventoryAccountName = financialAccount.name!;
+            inventoryAccountProperties = financialAccount.properties ?? {};
+            inventoryAccountArchived = financialAccount.archived ?? false;
+            inventoryAccountGroups = financialAccount.groups ?? [];
         }
-        console.log(`ERROR (getConnectedGoodAccount): financialAccount is missing required fields`);
-        return undefined;
+
+        let inventoryAccount = new Account(inventoryBook)
+            .setName(inventoryAccountName)
+            .setType(AccountType.ASSET)
+            .setProperties(inventoryAccountProperties)
+            .setArchived(inventoryAccountArchived);
+
+        if (inventoryAccountGroups.length > 0) {
+            for (const group of inventoryAccountGroups) {
+                let inventoryGroup = await inventoryBook.getGroup(group.name);
+                if (!inventoryGroup) {
+                    inventoryGroup = await new Group(inventoryBook)
+                        .setName(group.name ?? '')
+                        .setParent(await inventoryBook.getGroup(group.parent?.name))
+                        .setProperties(group.properties ?? {})
+                        .setHidden(group.hidden ?? false)
+                        .create()
+                        ;
+                }
+                if (inventoryGroup) {
+                    inventoryAccount.addGroup(inventoryGroup);
+                }
+            }
+        }
+        inventoryAccount = await inventoryAccount.create();
+
+        return inventoryAccount;
     }
 
 }
