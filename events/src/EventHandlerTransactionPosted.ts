d455b20429a7c912cffce690a833ad32902aedc9
Bruno Coelho <bbcoelho@gmail.com>
2025-04-02 09:54:46 -0700

    feat: handle transactions posted in the inventory book

--- events/src/EventHandlerTransactionPosted.ts
+++ events/src/EventHandlerTransactionPosted.ts
@@ -5,26 +5,26 @@ import { isInventoryBook, uncheckAndTrash } from "./BotService.js";
 export class EventHandlerTransactionPosted {
 
 	async handleEvent(event: bkper.Event): Promise<Result> {
-		// const eventBook = await Bkper.getBook(event.bookId!);
+		const eventBook = await Bkper.getBook(event.bookId!);
 
-		// // prevent response to transactions posted in the inventory book
-		// if (isInventoryBook(eventBook)) {
-		// 	// delete posted transaction and warn the user
-		// 	if (event.data) {
-		// 		if (!event.data.object) {
-		// 			return { result: false };
-		// 		}
-		// 		const operation = event.data.object as bkper.TransactionOperation;
-		// 		const transactionPayload = operation.transaction;
-		// 		const transaction = (await eventBook.listTransactions(transactionPayload!.id!)).getFirst();
-		// 		if (transaction) {
-		// 			await uncheckAndTrash(transaction);
-		// 		}
-		// 		const warningMsg = `You can't post directly in the Inventory book. Transaction deleted.`;
+		// prevent response to transactions posted in the inventory book
+		if (isInventoryBook(eventBook)) {
+			// delete posted transaction and warn the user
+			if (event.data) {
+				if (!event.data.object) {
+					return { result: false };
+				}
+				const operation = event.data.object as bkper.TransactionOperation;
+				const transactionPayload = operation.transaction;
+				const transaction = (await eventBook.listTransactions(transactionPayload!.id!)).getFirst();
+				if (transaction) {
+					await uncheckAndTrash(transaction);
+				}
+				const warningMsg = `You can't post directly in the Inventory book. Transaction deleted.`;
 
-		// 		return { warning: warningMsg };
-		// 	}
-		// }
+				return { warning: warningMsg };
+			}
+		}
 		return { result: false };
 	}
 
